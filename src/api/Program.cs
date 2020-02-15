2767e1996e95bdccd369b004dba400d6cdb4ef8e
Jerreck McWilliams <jerreck.mcwilliams@allsynx.com>
2020-02-15 17:07:30 -0600

    changed solution structure and added more to readme

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace hc_pcf
+namespace api
 {
     public class Program
     {
--- src/api/Startup.cs
+++ src/api/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace hc_pcf
+namespace api
 {
     public class Startup
     {
